440769a619e595083ac98a36f68559c086e4b9df
Marcos Verício <marcosvericio@gmail.com>
2023-03-11 11:24:13 -0300

    Trilha Java Básico

---
+++ Trilha Java Basico/java-tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,37 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        // tipos primitivos:
+
+        // double salarioMinimo = 2500; assim é 2500 reais, 2.500 seria igual a 2.50
+        // reais.
+
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 - numeroNormal; esse é um exemplo di tipagem do Java. Um
+        // INT pode guardar um SHORT
+        // mas um SHORT não pode guardar um INT. A não ser que seja feito um CASTING.
+
+        // CONSTANTES: abaixo temos um exemplo de valores que não podem ser alterados
+        // quando usamos uma CONTANTE,
+        // nesse caso adicionando o termo FINAL antes da variável garantimos que o valor
+        // inserido não possa
+        // ser utilizado em nenhuma outra variável de mesmo nome.
+
+        // int numero = 5;
+        // numero = 10;
+
+        // System.out.println(numero); Aqui ele vai imprimir o segundo número guardado,
+        // já que não há impedimento.
+
+        // final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 10.75; Aqui ele não imprimirá 10.75 porque a CONSTANTE FINAL
+        // foi informada acima.
+
+        // String meuNome = "Marcos Verício"; Exemplo de String, variável para TEXTO.
+
+        // System.out.println(meuNome);
+
+    }
+}
