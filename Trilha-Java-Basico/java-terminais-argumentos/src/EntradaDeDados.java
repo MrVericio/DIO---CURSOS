b73701a13aec0af876d99a59b028fa543edde5e2
Marcos Verício <marcosvericio@gmail.com>
2023-03-18 12:01:57 -0300

    Trilha Java Basico

---
+++ Trilha-Java-Basico/java-terminais-argumentos/src/EntradaDeDados.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EntradaDeDados {
+    public static void main(String[] args) {
+        // Criando o ojeto Scanner - Exemplo de Entrada de Dados
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        // Imprimindo os dados obtidos pelo usuário através do Scanner
+        System.out.println("Oá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+    }
+}
