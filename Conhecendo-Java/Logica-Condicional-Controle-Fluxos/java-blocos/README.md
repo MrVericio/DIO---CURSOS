2e6df46d721b89d325b87fbf0d2e1c077a097345
Marcos Verício <marcosvericio@gmail.com>
2023-03-24 11:35:40 -0300

    Conhecendo o JAVA

---
+++ Conhecendo-Java/Logica-Condicional-Controle-Fluxos/java-blocos/README.md
@@ -0,0 +1,15 @@
+## Blocos
+
+1 - Entendendo o que são Blocos
+2- Tipos de Blocos
+3 - Saber como usar os Blocos
+
+### O que são Blocos? 
+
+"É um grupo de 0 ou mais códigos os quais trabalham em conjunto para executar uma operação"
+
+#### Tipos:
+
+Locais: Dentro dos métodos
+Estáticos: Dentro das classes
+Instância: Dentro das classes
---
+++ Conhecendo-Java/Logica-Condicional-Controle-Fluxos/java-blocos/src/Blocos.java
@@ -0,0 +1,22 @@
+public class Blocos {
+    public static void main(String[] args) {
+        // Exemplos de Blocos
+
+        // Locais: {} abrir e fechar de chaves cria um Bloco
+
+        /* if (autorizado){
+            CarregaPerfil
+            DirecionarPaginaPrincial
+        } */ // Temos nesse if dois comandos
+
+        /* if (menorIdade)
+         *  DirecionarPaginaProibido
+         */ // Blocos com apenas uma linha de excução não necessitam de chaves {}
+
+         /* if (autorizado){
+            else {
+
+            }
+         } */ // Exemplo de dois Blocos if/else
+    }
+}
