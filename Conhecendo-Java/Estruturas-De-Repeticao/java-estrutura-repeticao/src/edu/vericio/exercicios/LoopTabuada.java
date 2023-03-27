866649bae70632ffca4e0d57048aec18b7d0c8a0
Marcos Verício <marcosvericio@gmail.com>
2023-03-27 12:02:17 -0300

    Conhecendo o JAVA

--- Conhecendo-Java/Estruturas-De-Repeticao/java-estrutura-repeticao/src/edu/vericio/exercicios/LoopTabuada.java
+++ Conhecendo-Java/Estruturas-De-Repeticao/java-estrutura-repeticao/src/edu/vericio/exercicios/LoopTabuada.java
@@ -17,7 +17,15 @@ public class LoopTabuada {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Tabuada: ");
+
+        int tabuada = sc.nextInt();
         
+        System.out.println("Tabuada de: " + tabuada);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(tabuada + " x " + i + " = " + (tabuada * i));
+        }
         sc.close();
     }
 }
