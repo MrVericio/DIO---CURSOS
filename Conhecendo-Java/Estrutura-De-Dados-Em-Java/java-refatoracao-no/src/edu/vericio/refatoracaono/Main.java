38f6119a2e6ac12f6b2d829759b006851e60f219
Marcos Verício <marcosvericio@gmail.com>
2023-04-03 09:57:52 -0300

    Conhecendo o JAVA

---
+++ Conhecendo-Java/Estrutura-De-Dados-Em-Java/java-refatoracao-no/src/edu/vericio/refatoracaono/Main.java
@@ -0,0 +1,33 @@
+package edu.vericio.refatoracaono;
+
+import javax.print.DocFlavor.STRING;
+
+public class Main {
+    public static void main(String[] args) {
+
+        No<String> no1 = new No<>("Conteúdo no1");
+
+        No<String> no2 = new No<>("Conteúdo no2");
+        no1.setProximoNo(no2);
+
+        No<String> no3 = new No<>("Conteúdo no3");
+        no2.setProximoNo(no3);
+
+        No<String> no4 = new No<>("Conteúdo no4");
+        no3.setProximoNo(no4);
+
+        // no1 -> no2 -> no3 -> no4 -> null / Um nó apontando pro outro até terminal no
+        // no4
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+
+        System.out.println("----------------------");
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+    }
+}
