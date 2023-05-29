ac81354631892f1f1bccd0572f0ad5e5dd187a3f
Marcos Verício <marcosvericio@gmail.com>
2023-05-29 18:36:01 -0300

    Tipos de Dados

---
+++ BootCamp - Python/Conhecendo a Linguagem/TiposDeDados.py
@@ -0,0 +1,19 @@
+print(11 + 10 + 1000)
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print("Python")
+
+
+
+
+int1 = 10
+int2 = 20
+str1 = "Marcos"
+str2 = "tem"
+str3 = "anos"
+
+resultado = int1 + int2
+print(str1, str2, str3 , resultado)
+
+
