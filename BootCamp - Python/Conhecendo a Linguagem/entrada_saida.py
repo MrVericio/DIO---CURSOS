ded6ff35bf24ef9829c6916a37d5da733375c84b
Marcos Verício <marcosvericio@gmail.com>
2023-05-30 17:02:29 -0300

    Funções de Entrada e Saída

---
+++ BootCamp - Python/Conhecendo a Linguagem/entrada_saida.py
@@ -0,0 +1,8 @@
+# Funções de Entrada e Saída
+# Ex: 
+nome = input("Escreva seu nome: ") # recebe nome
+sobrenome = input("Escreva seu sobrenome: ") # recebe sobrenome
+
+print("Meu nome completo é:", nome, sobrenome) # imprime o nome e o sobrenome na mesma linha
+print("Meu nome completo é:", nome, sobrenome, end='...\n') # imprime o mesmo adicionando ... e uma quebra de linha \n
+print("Meu nome completo é:", nome, sobrenome, sep='_') # imprimero o mesmo e transforma a separação no que estiver entre ' '
