4cca56462fc7d961545cb14867be39c892018d1c
Marcos Verício <marcosvericio@gmail.com>
2023-05-30 10:32:32 -0300

    Variáveis e Constantes

---
+++ BootCamp - Python/Conhecendo a Linguagem/VariaveisEConstantes.py
@@ -0,0 +1,14 @@
+# Exemplo de variáveis 
+
+idade = 30
+nome = 'Marcos'
+print(f'Meu nome é {nome} e eu tenho {idade} anos(s) de idade')
+
+# Exemplo de varável na mesma linha
+idade, nome = (30, 'Rayane') # O abrir e fechar () é opcional. Os valores devem ser separados por vírgula e em ordem.
+print(f'Meu nome é {nome} e eu tenho {idade} anos(s) de idade')
+
+# BOAS PRÁTICAS
+# O padrão de nomes deve ser sneak case. Ex: total_compra, valor_total, nome_completo / tudo em minúsculo
+# Escolher nomes sugestivos. Ex: saque, saldo, total, resultado, limite, cheque, 
+# Nome de constantes todo em maiúsculo. Ex: MINHA_CONSTANTE = 1 / Esse valor não será alterado
