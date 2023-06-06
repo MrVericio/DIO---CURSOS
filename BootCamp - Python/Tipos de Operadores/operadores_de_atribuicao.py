5565d8a9aa9e5dbc8158ff6f61b2a6b415fffbe7
Marcos Verício <marcosvericio@gmail.com>
2023-06-06 20:30:28 -0300

    Operadores de Atribuição

---
+++ BootCamp - Python/Tipos de Operadores/operadores_de_atribuicao.py
@@ -0,0 +1,39 @@
+# Atribuição simples
+# Ex
+saldo = 500
+print("Atribuição simples:", saldo)
+
+# Atirbuição com adição
+saldo = 500
+saldo += 200
+print("Atribuição com adição:", saldo)
+
+# Atirbuição com subtração
+saldo = 500
+saldo -= 200
+print("Atribuição com subtração:", saldo)
+
+# Atirbuição com multiplicação
+saldo = 500
+saldo *= 200
+print("Atribuição com multiplicação:", saldo)
+
+# Atirbuição com divisão
+saldo = 500
+saldo /= 200
+print("Atribuição com divisão:", saldo)
+
+# Atirbuição com divisão inteira
+saldo = 500
+saldo //= 200
+print("Atribuição com divisão inteira:", saldo)
+
+# Atirbuição com módulo
+saldo = 500
+saldo %= 200
+print("Atribuição com módulo:", saldo)
+
+# Atirbuição com exponenciação
+saldo = 500
+saldo **= 2
+print("Atribuição com exponenciação:", saldo)
