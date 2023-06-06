53b8872f839756daf88985d14af7597fd7e5ba45
Marcos Verício <marcosvericio@gmail.com>
2023-06-06 19:49:06 -0300

    Tipos de Operadores

---
+++ BootCamp - Python/Tipos de Operadores/tipos_de_operadores.py
@@ -0,0 +1,41 @@
+# Tipos de Operadores
+
+# Adição
+print("Soma (1 + 1):", 1 + 1) # imprime a soma de dois valores
+
+# Subtração
+print("Subtração (10 - 2):", 10 - 2) # imprime a subtração de dois valores
+
+# Multiplicação
+print("Multiplicação (4 * 3):", 4 * 3) # imprime a multiplicação de dois valores
+
+# Divisão
+print("Divisão (12 / 3):", 12 / 3) # imprime a divisão de dois valores com ponto float
+
+# Divisão Inteira
+print("Divisão Inteira (12 // 3):", 12 // 3) # imprime a divisão de dois valores sem o ponto float
+
+# Módulo
+print("Módulo (10 % 3):", 10 % 3) # imprime o resto da divisão
+
+# Exponenciação
+print("Exponenciação (2 ** 3):", 2 ** 3) # imprime a exponenciação, no exemplo 2 ao quadrado igual a 8
+
+''' 
+A definição indica a seguinte ordem como correta:
+Parênteses ()
+Expoêntes **   
+Multiplicaçãoes e Divisões * / (da esquerda para a direita)
+Somas e Subtrações + - (da esquerda para a direita)
+'''
+# Ex
+
+print("Subtração e Multiplicação (10 - 5 * 2):", 10 - 5 * 2) # subtração e multiplicação (esquerda para a direita)
+
+print("Parênteses ((10 - 5) * 2):", (10 - 5) * 2) # parênteses
+
+print("Exponenciação (10 ** 2 * 2):", 10 ** 2 * 2) # exponenciação
+
+print("Elevação (10 ** (2 * 2)):", 10 ** (2 * 2)) # elevação
+
+print("Divisão e Multiplicação (10 / 2 * 4):", 10 / 2 * 4) # divisão e multiplicação
