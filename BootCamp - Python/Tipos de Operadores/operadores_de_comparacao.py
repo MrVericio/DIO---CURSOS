cb4601cd96bba061e548045a530bd8d351e6eed1
Marcos Verício <marcosvericio@gmail.com>
2023-06-06 20:01:22 -0300

    Operadores de Comparação

---
+++ BootCamp - Python/Tipos de Operadores/operadores_de_comparacao.py
@@ -0,0 +1,25 @@
+# Operador de Igualdade
+# Ex
+saldo = 450
+saque = 200
+print("Saldo é igual a Saque?", saldo == saque)
+
+# Operador de Diferença
+# Ex
+saldo = 450
+saque = 200
+print("Saldo é idiferente de Saque?", saldo != saque)
+
+# Operador Maior e/ou Maior ou Igual
+# Ex
+saldo = 450
+saque = 200
+print("Saldo é maior que Saque?", saldo > saque)
+print("Saldo é maior ou igual a Saque?", saldo >= saque)
+
+# Operador Menor e/ou Menor ou Igual
+# Ex
+saldo = 450
+saque = 200
+print("Saldo é menor que Saque?", saldo < saque)
+print("Saldo é menor ou igual a Saque?", saldo <= saque)
