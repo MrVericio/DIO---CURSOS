0b7f10cb4b5b02a14bef5217bcfacfe5c3fee151
Marcos Verício <marcosvericio@gmail.com>
2023-06-06 22:51:02 -0300

    Operadores de Identidade

---
+++ BootCamp - Python/Tipos de Operadores/operadores_de_identidade.py
@@ -0,0 +1,14 @@
+# Operadores de Identidade
+# Ex
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200
+
+curso is nome_curso
+
+curso is not nome_curso
+
+saldo is limite
+
+print("Operador is:", saldo is limite)
+print("Operador is not:", saldo is not limite)
