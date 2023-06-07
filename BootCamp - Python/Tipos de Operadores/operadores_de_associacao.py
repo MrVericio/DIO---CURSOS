2e0e92c24d83ab37d3d3e78648afb5b339eda58e
Marcos Verício <marcosvericio@gmail.com>
2023-06-06 22:59:29 -0300

    Operadores de Associação

---
+++ BootCamp - Python/Tipos de Operadores/operadores_de_associacao.py
@@ -0,0 +1,15 @@
+# Operadores de Associação
+# Ex
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+"Python" in curso
+
+"maçã" not in frutas
+
+200 in saques
+
+print("Python" in curso)
+print("maçã" not in frutas)
+print(200 in saques)
