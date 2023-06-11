18ebc645c5bce2d2148ebb785bd3372f6ecfc437
Marcos Verício <marcosvericio@gmail.com>
2023-06-11 15:51:56 -0300

    If aninhado

--- BootCamp - Python/Estruturas Condicionais e de Repetição/estrutura_condicional_aninhada.py
+++ BootCamp - Python/Estruturas Condicionais e de Repetição/estrutura_condicional_aninhada.py
@@ -3,16 +3,18 @@ conta_normal = True
 conta_universitaria = False
 
 saldo = 2000
-saque = 200
+saque = 3500
 
-cheque_especial = 3500
+cheque_especial = 500
 
 
 if conta_normal:
     if saldo >= saque:
-        print("saque realizado com sucesso!")
+        print("Saque realizado com sucesso!")
     elif saque <= (saldo + cheque_especial):
         print("Saque realizado com uso de cheque especial!")
+    else:
+        print("Não foi possível realizar o saque!")
 
 elif conta_universitaria:
     if saldo >= saque:
